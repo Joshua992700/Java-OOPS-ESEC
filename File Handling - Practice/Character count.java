81e2afb903ee9a6a48e402772b307226b7d26c69
Joshua992700 <136467417+Joshua992700@users.noreply.github.com>
2024-04-14 15:04:41 +0530

    Update Character count.java

--- File Handling - Practice/Character count.java
+++ File Handling - Practice/Character count.java
@@ -1,35 +1,37 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-
-public class CharacterCount {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
+public class Program
+{
+    public static void main(String[] args) throws Exception 
+    {
+        int cnt=0;
+        String s;
+        char[] buffer;
+        File f1=new File("input.txt");
+        FileReader fr = new FileReader(f1);
+        BufferedReader bfr = new BufferedReader(fr);
         Scanner sc = new Scanner(System.in);
-        String fileName = "input.txt";
-        char searchChar = sc.next().charAt(0);
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            int charCount = 0;
-            int totalCharacters = 0;
-            int c;
-            while ((c = reader.read()) != -1) {
-                char ch = Character.toLowerCase((char) c);
-                if (ch == Character.toLowerCase(searchChar)) {
-                    charCount++;
+        char letter = sc.next().charAt(0);
+        while((s=bfr.readLine())!=null)
+        {
+            buffer=s.toCharArray();
+            for (char chr: buffer)
+            {
+                if (chr==letter)
+                {
+                    cnt++;
                 }
-                totalCharacters++;
             }
-
-            if (charCount > 0) {
-                System.out.println("Character: " + searchChar + " found! Count : " + charCount);
-            } 
-            if (charCount == 0) {
-                System.out.println("Character not found!");
-            }
-        } 
-        catch (IOException e) {
-            System.out.println("An error occurred while reading the file: " + e.getMessage());
         }
+        if(cnt==0)
+        {
+            System.out.println("Character not found!");
+            
+        }
+        else
+        {
+            System.out.println("Character: "+letter+" found! Count : "+cnt);
+        }
+        fr.close();
     }
 }
